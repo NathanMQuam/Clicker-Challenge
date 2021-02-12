2acad6e20ea2b020787bf951f4d222ed8316bbf7
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-02-12 16:09:44 -0700

    Created upgrades

---
+++ definitions.js
@@ -0,0 +1,87 @@
+// NOTE Number Shortening Letters
+
+// 10^[#] : "x"
+// 40,000 -> 40k
+const siPrefix = {
+   3: "k",
+   6: "M",
+   9: "G"
+}
+
+// NOTE Auto-Clicker Upgrades
+// #region
+ 
+// #region Fossil Fuels
+
+const steamEngine = {
+   name: "Steam Engine",
+   price: 100,
+   production: 1,
+   icon: "",
+   upgrades: [
+      {
+         name: "Safety Valve",
+         price: 1000,
+         description: "Prevents boilers from getting over-pressurized and exploding.",
+         modifier: 1,
+         icon: ""
+      }, {
+         name: "Lubricant",
+         price: 300,
+         description: "Increase the efficiency of your Steam Engines.",
+         modifier: 0.5,
+         icon: ""
+      }
+   ]
+}
+
+const coalTrain = {
+   name: "Coal Locomotive",
+   price: 1000,
+   production: 25,
+   icon: "",
+   upgrades: [
+      {
+         name: "Precision Engineering",
+         price: 5000,
+         description: "Increase the efficiency of your locomotives.",
+         modifier: 5,
+         icon: ""
+      },{
+         name: "Diesel Engines",
+         price: 25000,
+         description: "No need to carry water around anymore, drastically increase weight capacity.",
+         modifier: 15,
+         icon: ""
+      }
+   ]
+}
+
+const gasPowerPlant = {
+   name: "Fossil Fuel Power Plant",
+   price: 10000,
+   production: 500,
+   icon: ""
+}
+// #endregion
+// #endregion
+
+
+// NOTE Manual Click Upgrades
+// #region
+const plusOneClick = {
+   name: "+1 W/Click",
+   price: 50,
+   modifier: 1,
+   icon: ""
+}
+
+const doubleClickValue = {
+   name: "*2 W/Click",
+   price: 500,
+   modifier: 1,
+   icon: ""
+}
+
+
+// #endregion
