0e9c5f3f7abf6d75513a407cc2fd70273e25db50
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-02-12 17:08:33 -0700

    Upgrades

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ let clickModifiers = {
    doubles: 0
 }
 
+let autoInventory = [
+   {
+      clicker: steamEngine,
+      num: 1,
+      upgrades: [true, false]
+   }
+]
+
 const statsElem = document.getElementById("stats")
 const ACStoreElem = document.getElementById("auto-clicker-store")
 const AUStoreElem = document.getElementById("auto-upgrade-store")
@@ -18,6 +26,38 @@ function clickAction() {
    draw()
 }
 
+function purchase(upgrade) {
+   
+}
+
+
+
+// Gathers all the auto-clickers and determines how many Watts to produce every second
+function autoClick() {
+   // console.log("Auto Interval!");
+   // debugger
+   // let wattsPerSec = 0;
+   // for(let i = 0; i < autoInventory.length; i++) {
+   //    const upgrade = autoInventory[i]
+   //    console.log(upgrade.clicker.production, upgrade.num);
+
+   //    for(let j = 0; j < upgrade.upgrades.length; i++) {
+   //       console.log(upgrade.upgrades[i]);
+   //       // if(upgrade.upgrades[i])
+   //    }
+   // }
+}
+
+
+
 function draw() {
    statsElem.innerText = "Watts: " + watts
 }
+
+
+
+
+
+
+setInterval(autoClick, 1000)
+draw()
